2b3b43877faf885bbf6070be0d88459894960eb8
Louis <louis.bouddhou@gmail.com>
2016-01-20 15:25:06 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  setBindings();
+});
+
+function setBindings() {
+
+
+	  	$("#mynav a").click(function(e) {
+		  		e.preventDefault();
+		  		var sectionID = e.currentTarget.id + "Section";
+		      	// alert('button id ' + e.currentTarget.id);
+
+		      	$("html body").animate({
+			      		scrollTop: $("#" + sectionID).offset().top
+		      	}, 1000)
+
+  		})
+
+}
+
+
