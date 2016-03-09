93ff6b4462f55fd90b3c1f61e8a1eab0d8345c65
Louis <louis.bouddhou@gmail.com>
2016-03-09 02:01:13 -0700

    Updating gh-pages

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,8 @@ $(function() {
 	smoothScroll(300);
 	projectBelt();
 	workLoad();
+	backToTop();
+	// scrollEnd();
 
 	$('.thumb-unit').click(function(e){
         e.preventDefault();
@@ -20,6 +22,8 @@ $(function() {
         e.preventDefault();
         projectBelt();
     });
+
+    $('html, body').animate({scrollTop:$(document).height()}, 1000);
 
 });
 
@@ -75,6 +79,68 @@ function workLoad() {
 		$('.project-title').text(newTitle);
 
 
+	});
+}
+
+function backToTop() {
+	$(document).ready(function() {
+			// Show or hide the sticky footer button
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 200) {
+					$('.go-top').fadeIn(200);
+				} else {
+					$('.go-top').fadeOut(200);
+				}
+			});
+			
+			// Animate the scroll to top
+			$('.go-top').click(function(event) {
+				event.preventDefault();
+				
+				$('html, div').animate({scrollTop: 0}, 300);
+			})
+		});
+}
+
+function scrollEnd() {
+			// 	$(
+			//   function($)
+			//   {
+			//     $('#projectsSection').bind('scroll', function()
+			//       {
+			//         if($(this).scrollTop() + $(this).innerHeight()>=$(this)[0].scrollHeight)
+			//         {
+			//           alert('end reached');
+			//         }
+			//       })
+			// }
+			// );
+	$('#projectsSection').on('DOMMouseScroll mousewheel', function(ev) {
+	    var $this = $(this),
+	        scrollTop = this.scrollTop,
+	        scrollHeight = this.scrollHeight,
+	        height = $this.height(),
+	        delta = (ev.type == 'DOMMouseScroll' ?
+	            ev.originalEvent.detail * -40 :
+	            ev.originalEvent.wheelDelta),
+	        up = delta > 0;
+
+	    var prevent = function() {
+	        ev.stopPropagation();
+	        ev.preventDefault();
+	        ev.returnValue = false;
+	        return false;
+	    }
+	    
+	    if (!up && -delta > scrollHeight - height - scrollTop) {
+	        // Scrolling down, but this will take us past the bottom.
+	        $this.scrollTop(scrollHeight);
+	        return prevent();
+	    } else if (up && delta > scrollTop) {
+	        // Scrolling up, but this will take us past the top.
+	        $this.scrollTop(0);
+	        return prevent();
+	    }
 	});
 }
 
