e8eb7cd521bb289f3b45095bce104098ae4dd6e3
Louis <louis.bouddhou@gmail.com>
2016-03-24 02:29:01 -0600

    Added js script for IE moving background image

--- js/functions.js
+++ js/functions.js
@@ -24,6 +24,18 @@ $(function() {
         
     });
 
+    if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
+        $('body').on("mousewheel", function () {
+            // remove default behavior
+            event.preventDefault(); 
+
+            //scroll without smoothing
+            var wheelDelta = event.wheelDelta;
+            var currentScrollPosition = window.pageYOffset;
+            window.scrollTo(0, currentScrollPosition - wheelDelta);
+        });
+	}
+
 });
 
 function smoothScroll (duration) {
